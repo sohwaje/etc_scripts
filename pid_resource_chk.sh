0a68e8e452af9b91ea09b3a840c35115bcdab107
sigongweb <kisssulran@i-screammedia.com>
2019-12-11 12:13:33 +0900

    commit

--- pid_resource_chk.sh
+++ pid_resource_chk.sh
@@ -20,7 +20,7 @@ OAUTH2CLIENTPID=$(ps -ef | grep $OAUTH2CLIENT_NAME | grep -v grep | awk '{print 
 ADMINPID=$(ps -ef | grep $ADMIN_NAME | grep -v grep | awk '{print $2}')
 FILEPID=$(ps -ef | grep $FILE_NAME | grep -v grep | awk '{print $2}')
 CRAWLERPID=$(ps -ef | grep $CRAWLER_NAME | grep -v grep | awk '{print $2}')
-UIPID=$(ps -ef | grep UI_NAME | grep -v grep | awk '{print $2}')
+UIPID=$(ps -ef | grep $UI_NAME | grep -v grep | awk '{print $2}')
 
 # [4] APP의 PID의 메모리와 CPU 사용률
 COUNT=10
@@ -35,8 +35,10 @@ if [ -z $APIPID ]
 then
   echo "Hi-class-api 앱이 없음"
 else
+  echo "========================================"
   echo "API:" $(ps -p $APIPID -o %cpu)
   echo "API:" $(ps -p $APIPID -o %mem)
+  echo "========================================"
   echo
 fi
 
@@ -45,8 +47,10 @@ if [ -z $CHATPID ]
 then
   echo "hi-class-chat 앱이 없음"
 else
+  echo "========================================"
   echo "CHAT:" $(ps -p $CHATPID -o %cpu)
   echo "CHAT:" $(ps -p $CHATPID -o %mem)
+  echo "========================================"
   echo
 fi
 # OAUTH2
@@ -54,8 +58,10 @@ if [ -z $OAUTH2PID ]
 then
   echo "hi-class-oauth2 앱이 없음"
 else
+  echo "========================================"
   echo "oauth2:" $(ps -p $OAUTH2PID -o %cpu)
   echo "oauth2:" $(ps -p $OAUTH2PID -o %mem)
+  echo "========================================"
   echo
 fi
 
@@ -64,8 +70,10 @@ if [ -z $OAUTH2CLIENTPID ]
 then
   echo "hi-class-oauth2-client 앱이 없음"
 else
+  echo "========================================"
   echo "oauth2-client:" $(ps -p $OAUTH2CLIENTPID -o %cpu)
   echo "oauth2-client:" $(ps -p $OAUTH2CLIENTPID -o %mem)
+  echo "========================================"
   echo
 fi
 
@@ -74,8 +82,10 @@ if [ -z $ADMINPID ]
 then
   echo "hi-class-ui-admin 앱이 없음"
 else
+  echo "========================================"
   echo "admin:" $(ps -p $ADMINPID -o %cpu)
   echo "admin:" $(ps -p $ADMINPID -o %mem)
+  echo "========================================"
   echo
 fi
 
@@ -84,8 +94,10 @@ if [ -z $FILEPID ]
 then
   echo "hi-class-file 앱이 없음"
 else
+  echo "========================================"
   echo "file:" $(ps -p $FILEPID -o %cpu)
   echo "file:" $(ps -p $FILEPID -o %mem)
+  echo "========================================"
   echo
 fi
 
@@ -94,8 +106,10 @@ if [ -z $CRAWLERPID ]
 then
   echo "hi-class-crawler 앱이 없음"
 else
+  echo "========================================"
   echo "crawler:" $(ps -p $CRAWLERPID -o %cpu)
   echo "crawler:" $(ps -p $CRAWLERPID -o %mem)
+  echo "========================================"
   echo
 fi
 
@@ -104,8 +118,10 @@ if [ -z $UIPID ]
 then
   echo "hi-class-ui 앱이 없음"
 else
+  echo "========================================"
   echo "front:" $(ps -p $UIPID -o %cpu)
   echo "front:" $(ps -p $UIPID -o %mem)
+  echo "========================================"
   echo
 fi
 sleep 2
