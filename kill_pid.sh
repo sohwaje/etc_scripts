cd359e4c096e0db2d999ce61187f171f198162c1
sigongweb <kisssulran@i-screammedia.com>
2020-03-26 13:28:39 +0900

    APP 이름으로 pid를 찾아서 kill하는 스크립트

--- kill_pid.sh
+++ kill_pid.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-APP_NAME="$(ls /home/sigongweb/apps/api)"
-
-PID=$(get_PID)
-
-kill -9 $PID
+APP_NAME="$(ls /home/sigongweb/apps/front)"
 
 get_PID()
 {
     ps ux | grep ${APP_NAME} | grep -v grep | awk '{print $2}'
 }
+
+PID=$(get_PID)
+  echo "APP name is ${APP_NAME}"
+  sleep 1
+  echo "kill -9 ${PID}"
+
+kill -9 ${PID}
