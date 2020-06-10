c24307bf9a427fcc9705be07068c3704b1bf9121
sigongweb <kisssulran@i-screammedia.com>
2020-06-10 15:06:18 +0900

    update

---
+++ kubernetes/registry_purge.sh
@@ -0,0 +1,22 @@
+# Environment variable for container command line
+PURGE_CMD="acr purge --filter 'hello-world:.*' \
+  --ago 7d"
+
+az acr task create --name purgeTask \
+  --cmd "$PURGE_CMD" \
+  --schedule "0 0 * * *" \
+  --registry myregistry \
+  --context /dev/null
+
+
+
+
+# Environment variable for container command line
+PURGE_CMD="acr purge \
+  --filter 'hi-class-api:.*' --filter 'samples/devimage2:.*' \
+  --ago 0d --untagged --dry-run"
+
+az acr run \
+  --cmd "$PURGE_CMD" \
+  --registry myregistry \
+  /dev/null
