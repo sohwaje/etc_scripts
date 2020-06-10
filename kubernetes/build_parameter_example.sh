c672749858d23e845d2bab58b346721ed7ab8e27
sigongweb <kisssulran@i-screammedia.com>
2020-06-10 13:58:12 +0900

    update

--- kubernetes/build_parameter_example.sh
+++ kubernetes/build_parameter_example.sh
@@ -7,3 +7,16 @@ docker push $WEB_IMAGE_NAME
 # Update kubernetes deployment with new image.
 WEB_IMAGE_NAME="${ACR_LOGINSERVER}/azure-vote-front:kube${BUILD_NUMBER}"
 kubectl set image deployment/azure-vote-front azure-vote-front=$WEB_IMAGE_NAME --kubeconfig /var/lib/jenkins/config
+
+
+
+##### http://api.hiclass.net #######
+# Build new image and push to ACR.
+WEB_IMAGE_NAME="${ACR_LOGINSERVER}/hi-class-api:${BUILD_NUMBER}"
+docker image build -t $WEB_IMAGE_NAME .
+docker login ${ACR_LOGINSERVER} -u ${ACR_ID} -p ${ACR_PASSWORD}
+docker push $WEB_IMAGE_NAME
+
+# Deploy new Image & Replace buid number
+sed -i "s/hiclass.azurecr.io\/hi-class-api.*/hiclass.azurecr.io\/hi-class-api:${BUILD_NUMBER}/g" hiclass-stage-api.yml
+kubectl apply -f hiclass-stage-api.yml --namespace ingress-basic --kubeconfig /var/lib/jenkins/config
