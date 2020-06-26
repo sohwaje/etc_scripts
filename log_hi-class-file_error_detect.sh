77f013ae58c0bd75dc5b1ea8ef59389b02a9a799
sohwaje <kissenyo@gmail.com>
2020-06-26 15:29:42 +0900

    Update log_hi-class-file_error_detect.sh

--- log_hi-class-file_error_detect.sh
+++ log_hi-class-file_error_detect.sh
@@ -1,24 +1,44 @@
 #!/bin/sh
 # 로그의 특정 메시지가 기준치 이상으로 반복해서 찍히면 SLACK으로 Alert 메세지를 보냅니다.
 #LOG file
-LOG="/home/sigongweb/apps/logs/hi-class-file.log.2020-06-14.5"
-# Slack 주소
-WEBHOOK_ADDRESS='https://hooks.slack.com/services/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'
-# 날짜
-DATE=$(date '+%Y-%m-%d %H:%M:%S')
-# json 형식의 ALERT 메시지
-ALERT_TEXT="{\"text\": \"MSG: scheduling Error Date:  $DATE\"}"
+LOG="/home/sigongweb/apps/logs/hi-class-file.log"
+
 # 특정 메시지 개수를 담은 변수
 NUM=$(tail -n 20 $LOG | grep 'scheduling-1' | wc -l)
+
+# Slack 주소
+WEBHOOK_ADDRESS='https://hooks.slack.com/services/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'
+
+# 날짜
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+
+# 슬랙으로 메시지 보내기 함수
+function slack_message(){
+    # $1 : message
+    # $2 : true=good, false=danger
+
+    COLOR="danger"
+    if $2 ; then
+        COLOR="good"
+    fi
+    curl -s -d 'payload={"attachments":[{"color":"'"$COLOR"'","pretext":"<!channel> *hi-class-file*","text":"*HOST* : '"$HOSTNAME"' \n*MESSAGE* : '"$1"'"}]}' $WEBHOOK_ADDRESS > /dev/null 2>&1
+}
+
+# json 형식의 ALERT 메시지
+ALERT_TEXT="{\"text\": \"MSG: scheduling Error Date:  $DATE\"}"
+
 
 # 임시 파일 삭제
 rm -f log.tmp
 
-# 로그 파일 개수 확인
-if [ $NUM -gt 10 ];
-then
+# 특정 로그 파일이 연속해서 10개 이상 찍히면 Slack으로 알림 전송
+if [ $NUM -gt 10 ]; then
+#if [ $NUM -ne 10 ]; then
   echo "Warning: $NUM  | Date: $DATE"
-  curl -X POST -H 'Content-type: application/json' --data "$ALERT_TEXT" $WEBHOOK_ADDRESS > /dev/null 2>&1
+  slack_message "$HOSTNAME hi-class-file server maybe FAULT!!!!. " false  # 성공
+  exit 0
 else
+#  slack_message "$HOSTNAME NORMARL" true             # 실패
   echo "Normal : $NUM | DATE: $DATE"
+  exit 1
 fi
